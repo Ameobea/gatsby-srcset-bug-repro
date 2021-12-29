834e2d9ebb8c4e3ca8fd7e0e528cf808a4678d47
Casey Primozic <me@ameo.link>
2021-12-28 23:04:56 -0600

    Add minimal repro of `srcset` bug
     * This bug results in the following HTML getting generated: `<img class="gatsby-resp-image-image" alt="Chinese Salty Egg" title="" src="/static/8058f3f26913fea3b6a89a73344fe94a/828fb/salty_egg.jpg" srcset="/static/8058f3f26913fea3b6a89a73344fe94a/ff44c/salty_egg.jpg 158w /static/8058f3f26913fea3b6a89a73344fe94a/a6688/salty_egg.jpg 315w /static/8058f3f26913fea3b6a89a73344fe94a/828fb/salty_egg.jpg 630w /static/8058f3f26913fea3b6a89a73344fe94a/0ede0/salty_egg.jpg 945w /static/8058f3f26913fea3b6a89a73344fe94a/3ac88/salty_egg.jpg 1260w /static/8058f3f26913fea3b6a89a73344fe94a/e1596/salty_egg.jpg 2048w" sizes="(max-width: 630px) 100vw, 630px" loading="lazy" decoding="async" style="width: 100%; height: 100%; margin: 0px; vertical-align: middle; position: absolute; top: 0px; left: 0px; opacity: 1; transition: opacity 0.5s ease 0s; color: inherit; box-shadow: white 0px 0px 0px 400px inset;">`.  Note the lack of commas in the generated `srcset` attribute.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,13 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import rehypeReact from "rehype-react"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const renderAst = new rehypeReact({ createElement: React.createElement })
+  .Compiler
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -25,10 +29,7 @@ const BlogPostTemplate = ({ data, location }) => {
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
           <p>{post.frontmatter.date}</p>
         </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
+        <section itemProp="articleBody">{renderAst(post.htmlAst)}</section>
         <hr />
         <footer>
           <Bio />
@@ -80,7 +81,7 @@ export const pageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       id
       excerpt(pruneLength: 160)
-      html
+      htmlAst
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
